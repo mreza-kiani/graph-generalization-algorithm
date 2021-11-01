477df3fb575521a571284290700eba027d37c639
Mohammad Reza Kiani <mohammad.76kiani@gmail.com>
2021-11-01 01:02:33 -0600

    Add CompleteGraphTest.kt

---
+++ src/test/kotlin/ca/ucalgary/cs/comparison/CompleteGraphTest.kt
@@ -0,0 +1,77 @@
+package ca.ucalgary.cs.comparison
+
+import ca.ucalgary.cs.graph.Graph
+import ca.ucalgary.cs.graph.Node
+import kotlin.test.assertEquals
+
+class CompleteGraphTest : BaseCompareGraphTest() {
+    override fun initializeGraphs() {
+        val a = Node("A")
+        val b = Node("B")
+        val c = Node("C")
+
+        val g1d = Node("G1D")
+        val g1e = Node("G1E")
+        val g1f = Node("G1F")
+
+        val g2d = Node("G2D")
+        val g2e = Node("G2E")
+        val g2f = Node("G2F")
+        val g2g = Node("G2G")
+
+        graph1 = Graph(
+            nodes = listOf(a, b, c, g1d, g1e, g1f),
+            edges = mapOf(
+                a to listOf(b, c, g1d, g1e, g1f),
+                b to listOf(a, c, g1d, g1e, g1f),
+                c to listOf(a, b, g1d, g1e, g1f),
+                g1d to listOf(a, b, c, g1e, g1f),
+                g1e to listOf(a, b, c, g1d, g1f),
+                g1f to listOf(a, b, c, g1e, g1d),
+            )
+        )
+
+        graph2 = Graph(
+            nodes = listOf(a, b, c, g2d, g2e, g2f, g2g),
+            edges = mapOf(
+                a to listOf(b, c, g2d, g2e, g2f, g2g),
+                b to listOf(a, c, g2d, g2e, g2f, g2g),
+                c to listOf(a, b, g2d, g2e, g2f, g2g),
+                g2d to listOf(a, b, c, g2e, g2f, g2g),
+                g2e to listOf(a, b, c, g2d, g2f, g2g),
+                g2f to listOf(a, b, c, g2e, g2d, g2g),
+                g2g to listOf(a, b, c, g2e, g2d, g2f),
+            )
+        )
+    }
+
+    override fun checkCommonGraph(commonGraph: Graph) {
+        val a = Node("A")
+        val b = Node("B")
+        val c = Node("C")
+
+        val g1d = Node("G1D")
+        val g1e = Node("G1E")
+        val g1f = Node("G1F")
+
+        val g2d = Node("G2D")
+        val g2e = Node("G2E")
+        val g2f = Node("G2F")
+        val g2g = Node("G2G")
+
+        assertEquals(commonGraph.nodeVariables.size, 3)
+        assertEquals(commonGraph.edgeVariables.size, 6)
+//
+//        val nv1 = commonGraph.nodeVariables.first { it.graph1 == Graph(nodes = listOf(g1g), edges = mapOf()) }
+//        assertEquals(nv1.graph2, Graph(nodes = listOf(g2g1, g2g2), edges = mapOf(g2g1 to listOf(g2g2))))
+//
+//        val nv2 = commonGraph.nodeVariables.first { it.graph2 == Graph(nodes = listOf(g2d), edges = mapOf()) }
+//        assertEquals(nv2.graph1, Graph(nodes = listOf(g1d, g1h), edges = mapOf(g1d to listOf(g1h))))
+//
+//        val nv3 = commonGraph.nodeVariables.first { it.graph1 == Graph(nodes = listOf(g1e, g1f), edges = mapOf(g1e to listOf(g1f))) }
+//        assertEquals(nv3.graph2, Graph(nodes = listOf(g2e, g2f), edges = mapOf(g2e to listOf(g2f))))
+    }
+
+    override fun checkGraph1Diff(graph1Diff: Graph) {}
+    override fun checkGraph2Diff(graph2Diff: Graph) {}
+}
