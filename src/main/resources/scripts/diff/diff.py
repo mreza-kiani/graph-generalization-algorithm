1f880ba1c650e17ff9428367e043a4e54102d3a5
Mohammad Reza Kiani <mohammad.76kiani@gmail.com>
2022-05-17 11:34:33 -0600

    Complete diff tool script

--- src/main/resources/scripts/diff/diff.py
+++ src/main/resources/scripts/diff/diff.py
@@ -1,8 +1,7 @@
 import os
 import javalang
-from javalang.tree import FieldDeclaration, MethodDeclaration
 
-base_dir = "/home/mamareza/UofC/Thesis/Projects/JodaTime"
+base_dir = "/Users/mohammadreza/UofC/thesis/libraries"
 project1_name = "joda-time-2.0"
 project2_name = "joda-time-2.10.13"
 
@@ -27,22 +26,46 @@ def get_diff_info():
     return result
 
 
+def fill_version_info(result_part, file_path, project_name, version):
+    file_full_path = f"{base_dir}/{project_name}{file_path}"
+    file = open(file_full_path, "r")
+    file_content = file.read()
+    ast = javalang.parse.parse(file_content)
+    result_part[f'v{version}_size'] = os.path.getsize(file_full_path)
+    result_part[f'v{version}_lines'] = file_content.count("\n")
+    result_part[f'v{version}_methods'] = len(ast.types[0].methods)
+    result_part[f'v{version}_fields'] = len(ast.types[0].fields)
+
+
 def enrich_diff_info(diff_info):
     result = {'added': {}, 'removed': {}, 'changed': {}}
-    for file_path in diff_info['added']:
-        file_full_path = f"{project2_name}{file_path}"
-        file = open(f"{base_dir}/{file_full_path}", "r")
-        file_content = file.read()
-        ast = javalang.parse.parse(file_content)
-        result['added'][file_full_path] = {
-            'size': os.path.getsize(f"{base_dir}/{file_full_path}"),
-            'fields': len(ast.types[0].fields),
-            'methods': len(ast.types[0].methods)
-        }
+    for key in result:
+        for file_path in diff_info[key]:
+            result[key][file_path] = {
+                'v1_size': 0, 'v1_lines': 0, 'v1_fields': 0, 'v1_methods': 0,
+                'v2_size': 0, 'v2_lines': 0, 'v2_fields': 0, 'v2_methods': 0
+            }
+            if key != 'removed':
+                fill_version_info(result[key][file_path], file_path, project2_name, version=2)
+            if key != 'added':
+                fill_version_info(result[key][file_path], file_path, project1_name, version=1)
+    return result
 
+
+def sort_result_by(result, field_name):
+    for key in result:
+        result[key] = {k: v for k, v in sorted(
+            result[key].items(),
+            key=lambda item: abs(item[1][f'v1_{field_name}'] - item[1][f'v2_{field_name}']),
+            reverse=True
+        )}
 
 
 if __name__ == '__main__':
     diff_info = get_diff_info()
     enriched_diff_info = enrich_diff_info(diff_info)
-    print(diff_info)
+    sort_result_by(enriched_diff_info, field_name='size')
+    sort_result_by(enriched_diff_info, field_name='methods')
+    sort_result_by(enriched_diff_info, field_name='fields')
+    sort_result_by(enriched_diff_info, field_name='lines')
+    print(enriched_diff_info)
