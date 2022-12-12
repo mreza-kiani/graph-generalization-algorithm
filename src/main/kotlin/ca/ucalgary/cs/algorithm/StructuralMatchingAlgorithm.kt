0fd819faedddaa64a1b2c1d42282d17e8d021915
Mohammad Reza Kiani <mohammad.76kiani@gmail.com>
2022-12-12 15:28:27 -0700

    Remove extra code

--- src/main/kotlin/ca/ucalgary/cs/algorithm/StructuralMatchingAlgorithm.kt
+++ src/main/kotlin/ca/ucalgary/cs/algorithm/StructuralMatchingAlgorithm.kt
@@ -99,13 +99,13 @@ object StructuralMatchingAlgorithm {
         val compareBySimilarity: Comparator<Pair<String, Double>> = compareByDescending { it.second }
         val queue = PriorityQueue(compareBySimilarity)
 
-        graph1.nodes.reversed().forEachIndexed { g1Index, g1Node ->
+        graph1.nodes.forEachIndexed { g1Index, g1Node ->
             if (g1Node.isCommon)
                 return@forEachIndexed
-            graph2.nodes.reversed().forEachIndexed { g2Index, g2Node ->
+            graph2.nodes.forEachIndexed { g2Index, g2Node ->
                 if (g2Node.isCommon)
                     return@forEachIndexed
-                queue.add("${g1Node.completeName()}#${g2Node.completeName()}" to similarities[graph1.nodes.size-1 - g1Index][graph2.nodes.size-1 - g2Index])
+                queue.add("${g1Node.completeName()}#${g2Node.completeName()}" to similarities[g1Index][g2Index])
             }
         }
 
@@ -125,89 +125,12 @@ object StructuralMatchingAlgorithm {
         }
     }
 
-    private fun matchSameNodesOf(sourceGraph: Graph, targetGraph: Graph) {
-        // TODO: It's better to select the best nodes between 2 graphs for this, not the best in g1 and then go for g2
-        val sameNodes = extractSameNodes(sourceGraph).toMutableList()
-        val numberOfSameNodes = sameNodes.size
-        repeat(numberOfSameNodes) {
-            val g1Node =
-                sameNodes.maxByOrNull { node -> sourceGraph.degreeOf(node) + sourceGraph.degreeOfMatchedNodes(node) }!!
-            val potentialNodes = targetGraph.nodes.filter { it == g1Node }
-            val matchedNode = potentialNodes.associateWith { g2Node ->
-                val g1Edges = sourceGraph.allInAndOutEdgesOf(g1Node)
-                val g2Edges = targetGraph.allInAndOutEdgesOf(g2Node)
-                val degree = g1Edges.sumOf { g1Edge -> if (g1Edge in g2Edges) 1L else 0 } +
-                        g1Edges.sumOf { g1Edge -> g2Edges.filter { g2Edge -> g1Edge.isExactMatch(g2Edge) }.size }
-
-                degree.toDouble() / (sourceGraph.degreeOf(g1Node) + targetGraph.degreeOf(g2Node))
-            }.toList().maxByOrNull { (_, degree) -> degree }?.first
-
-            if (matchedNode != null)
-                applyMatchingInNames(g1Node, matchedNode)
-
-            sameNodes.remove(g1Node)
-        }
-    }
-
     private fun applyMatchingInNames(source: Node, matchedNode: Node) {
         source.name += "_${source.code}"
         source.isCommon = true
         matchedNode.name = source.name
         matchedNode.code = source.code
         matchedNode.isCommon = true
-    }
-
-    private fun extractSameNodes(graph: Graph) =
-        graph.nodes
-            .groupBy { it.name }
-            .filter { (_, list) -> list.size > 1 }
-            .values
-            .flatten()
-
-    fun unmatchIrrelevantParents(graph: Graph, dependentGraph: Graph) {
-        if (graph.edges.isEmpty())
-            return
-
-        val levels = mutableListOf<MutableList<Node>>()
-        dfs(node = graph.edges.keys.first(), level = 0, levels, graph)
-
-        if (levels.size <= 1)
-            return
-
-        for (i in levels.size - 2 downTo 0)
-            levels[i].forEach { node ->
-                val neighbors = graph.edgesOf(node)
-                if (!node.isCommon || neighbors.isEmpty())
-                    return@forEach
-                val matchedChildren =
-                    neighbors.filter { edge -> dependentGraph.nodes.any { edge.to.isExactMatch(it) } }.size
-                if (matchedChildren.toDouble() / neighbors.size < MATCHING_THRESHOLD)
-                    unmatchNode(node, dependentGraph)
-            }
-    }
-
-    private fun unmatchNode(node: Node, dependentGraph: Graph) {
-        val otherNode = dependentGraph.nodes.first { node.isExactMatch(it) }
-        applyUnMatchingInNames(node, otherNode)
-    }
-
-    private fun applyUnMatchingInNames(source: Node, matchedNode: Node) {
-        source.name += "_UM"
-        source.isCommon = false
-        matchedNode.isCommon = false
-    }
-
-    private fun dfs(node: Node, level: Int, levels: MutableList<MutableList<Node>>, graph: Graph) {
-        if (levels.any { seenNodes -> node in seenNodes })
-            return
-
-        if (levels.size <= level)
-            levels.add(mutableListOf())
-
-        levels[level].add(node)
-        graph.edgesOf(node).forEach { edge ->
-            dfs(node = edge.to, level = level + 1, levels, graph)
-        }
     }
 
     private fun dfs(node: Node, depth: MutableMap<Node, Int>, graph: Graph): Int {
--- src/main/kotlin/ca/ucalgary/cs/graph/Graph.kt
+++ src/main/kotlin/ca/ucalgary/cs/graph/Graph.kt
@@ -55,7 +55,6 @@ open class Graph(val nodes: List<Node>, val edges: Map<Node, List<Edge>>) : Edge
         fun compare(graph1: Graph, graph2: Graph): Triple<Graph, Graph, Graph> {
             markCommonNodes(graph1, graph2)
             alterNamesOfSameNodes(graph1, graph2)
-//            unmatchIrrelevantNodes(graph1, graph2)
             // TODO: Change variable names based on the structural similarities and then we have a ordinary generalization algorithm.
             // TODO: Having a matching-threshold to ignore some of the similarities
 
@@ -109,11 +108,6 @@ open class Graph(val nodes: List<Node>, val edges: Map<Node, List<Edge>>) : Edge
             val g2Diff = graph2 - commonGraph
 
             return Triple(commonGraph, g1Diff, g2Diff)
-        }
-
-        private fun unmatchIrrelevantNodes(graph1: Graph, graph2: Graph) {
-            StructuralMatchingAlgorithm.unmatchIrrelevantParents(graph1, graph2)
-            StructuralMatchingAlgorithm.unmatchIrrelevantParents(graph2, graph1)
         }
 
         private fun markCommonNodes(graph1: Graph, graph2: Graph) {
