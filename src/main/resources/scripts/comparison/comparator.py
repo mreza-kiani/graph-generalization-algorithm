9d7330dad1afa06c18322b87807ed6a3bc3768cc
Mohammad Reza Kiani <mohammad.76kiani@gmail.com>
2023-04-24 17:08:14 -0600

    Modify and add scripts to compare the AUAST results vs Generalizations'

--- src/main/resources/scripts/comparison/comparator.py
+++ src/main/resources/scripts/comparison/comparator.py
@@ -1,32 +1,49 @@
 from Levenshtein import *
+import sys
 
-from comparison.functions import read_inputs, raw_completeness, raw_compression, variable_ratio
+from functions import read_inputs, raw_completeness, raw_compression, variable_ratio
 
-dir_name = 'Assert8vs9'
-may, generalization = read_inputs(name1=f"data/{dir_name}/may.java", name2=f"data/{dir_name}/generalization.java")
-input1, input2 = read_inputs(name1=f"data/{dir_name}/input1.java", name2=f"data/{dir_name}/input2.java")
+f = open("summary.csv", "a")
 
-print('---------------------------------------Distance------------------------------------------------')
-print("\t                input1 vs input2:", distance(input1, input2))
-print("\tMay vs Generalization for input1:", distance(may, input1), 'vs', distance(generalization, input1))
-print("\tMay vs Generalization for input2:", distance(may, input2), 'vs', distance(generalization, input2))
-print("\t                         Average:", (distance(may, input1)+distance(may, input2))/2, 'vs',
-      (distance(generalization, input1) + distance(generalization, input2)) / 2)
-print("\t May and Generalization distance:", distance(may, generalization))
 
-print()
-print('--------------------------------------Completeness----------------------------------------------')
-print("\tMay vs Generalization for input1:", raw_completeness(may, input1), 'vs', raw_completeness(generalization, input1))
-print("\tMay vs Generalization for input2:", raw_completeness(may, input2), 'vs', raw_completeness(generalization, input2))
-print("\t                         Average:", (raw_completeness(may, input1) + raw_completeness(may, input2)) / 2,
-      'vs', (raw_completeness(generalization, input1) + raw_completeness(generalization, input2)) / 2)
+def compare(dir_name, input1_path, input2_path):
+    may, generalization = read_inputs(name1=f"{dir_name}/FinalAUAST.java", name2=f"{dir_name}/Generalization.java")
+    input1, input2 = read_inputs(name1=f"{input1_path}", name2=f"{input2_path}")
 
-print()
-print('--------------------------------------Compression-----------------------------------------------')
-print('\t           Compression of May:', raw_compression(may))
-print('\tCompression of Generalization:', raw_compression(generalization))
+    print('---------------------------------------Distance------------------------------------------------')
+    print("\t                  input1 vs input2:", distance(input1, input2))
+    print("\tAUAST vs Generalization for input1:", distance(may, input1), 'vs', distance(generalization, input1))
+    print("\tAUAST vs Generalization for input2:", distance(may, input2), 'vs', distance(generalization, input2))
+    print("\t                           Average:", (distance(may, input1) + distance(may, input2)) / 2, 'vs',
+          (distance(generalization, input1) + distance(generalization, input2)) / 2)
+    print("\t AUAST and Generalization distance:", distance(may, generalization))
 
-print()
-print('-------------------------------------Variable Ratio----------------------------------------------')
-print('\tInput1 - May vs Generalization:', variable_ratio(may, input1), 'vs', variable_ratio(generalization, input1))
-print('\tInput2 - May vs Generalization:', variable_ratio(may, input2), 'vs', variable_ratio(generalization, input2))
+    print()
+    print('--------------------------------------Completeness----------------------------------------------')
+    print("\tAUAST vs Generalization for input1:", raw_completeness(may, input1), 'vs',raw_completeness(generalization, input1))
+    print("\tAUAST vs Generalization for input2:", raw_completeness(may, input2), 'vs', raw_completeness(generalization, input2))
+    print("\t                           Average:", (raw_completeness(may, input1) + raw_completeness(may, input2)) / 2,
+          'vs', (raw_completeness(generalization, input1) + raw_completeness(generalization, input2)) / 2)
+
+    print()
+    print('--------------------------------------Compression-----------------------------------------------')
+    print('\tAUAST vs Generalization:', raw_compression(may), 'vs', raw_compression(generalization))
+
+    print()
+    print('-------------------------------------Variable Ratio----------------------------------------------')
+    print('\tInput1 - AUAST vs Generalization:', variable_ratio(may, input1), 'vs', variable_ratio(generalization, input1))
+    print('\tInput2 - AUAST vs Generalization:', variable_ratio(may, input2), 'vs', variable_ratio(generalization, input2))
+
+    f.write(f"{dir_name.split('/')[1]}, {dir_name.split('/')[2]}, {input1_path.split('/')[-1]} vs {input2_path.split('/')[-1]}, "
+            f"{(raw_completeness(may, input1) + raw_completeness(may, input2)) / 2}, {(raw_completeness(generalization, input1) + raw_completeness(generalization, input2)) / 2},"
+            f"{raw_compression(may)}, {raw_compression(generalization)},"
+            f"{(variable_ratio(may, input1) + variable_ratio(may, input2)) / 2}, {(variable_ratio(generalization, input1) + variable_ratio(generalization, input2)) / 2}\n")
+
+
+if __name__ == '__main__':
+    dir_name = sys.argv[1]
+    input1_path = sys.argv[2]
+    input2_path = sys.argv[3]
+    print(dir_name, input1_path, input2_path)
+    compare(dir_name, input1_path, input2_path)
+    f.close()
